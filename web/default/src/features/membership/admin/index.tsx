00d8148fbb8d577c7a595f1a822d32f52afc2967
MiLab <105380387+MiLab-Bit@users.noreply.github.com>
2026-08-06 02:30:09 +0800

    update web/default/src/features/membership/admin/index.tsx

--- web/default/src/features/membership/admin/index.tsx
+++ web/default/src/features/membership/admin/index.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2023-2026 OpenFastToken
+Copyright (C) 2023-2026 FastToken
 */
 import { useState, useEffect, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
