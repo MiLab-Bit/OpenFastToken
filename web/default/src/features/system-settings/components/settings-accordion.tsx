a5d5f0a32a992f14b4de48b512281aa0084f315c
MiLab <105380387+MiLab-Bit@users.noreply.github.com>
2026-08-06 02:38:07 +0800

    update web/default/src/features/system-settings/components/settings-accordion.tsx

--- web/default/src/features/system-settings/components/settings-accordion.tsx
+++ web/default/src/features/system-settings/components/settings-accordion.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2023-2026 OpenFastToken
+Copyright (C) 2023-2026 FastToken
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -14,7 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-For commercial licensing, please contact support@example.com
+For commercial licensing, please contact hello@fasttoken.example.com
 */
 import { cn } from '@/lib/utils'
 import {
